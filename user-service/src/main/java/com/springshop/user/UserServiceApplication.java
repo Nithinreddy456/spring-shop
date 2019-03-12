e01aefa33799e5bcf478b86d169e255155ecf984
Durga Prasad Narikalapa <ndurgaprasad10@gmail.com>
2019-03-12 07:20:28 +0530

    User service starter

---
+++ user-service/src/main/java/com/springshop/user/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springshop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
